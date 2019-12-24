1595fc11ee29489d565ec7ab8d9e1512e04fd636
KalilDev <kalildev@users.noreply.github.com>
2019-12-24 15:06:40 -0300

    seaside_web: Initial commit

---
+++ blueprint-templates/angular-component/__snakeCase_name___component.dart
@@ -0,0 +1,14 @@
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:seaside_web/src/components.dart';
+
+@Component(
+  selector: '{{kebabCase name}}',
+  styleUrls: ['{{snakeCase name}}_component.css'],
+  templateUrl: '{{snakeCase name}}_component.html',
+  directives: [
+  ],
+)
+class {{pascalCase name}} {
+  // TODO
+}
